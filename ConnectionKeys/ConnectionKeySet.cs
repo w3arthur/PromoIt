35be3e7179a40cfcf0208ec3a46bb8fe75905261
Arthur Zarankin <78190695+w3arthur@users.noreply.github.com>
2022-02-17 23:59:08 +0200

    Add files via upload

---
+++ ConnectionKeys/ConnectionKeySet.cs
@@ -0,0 +1,33 @@
+﻿namespace ConnectionKeys
+{
+     
+    public struct ConnectionKeySet
+    {
+        //set as git ignore
+        public static string MySQLKey_Server { get; } = "";
+        public static string MySQLKey_User { get; } = "";
+        public static string MySQLKey_Password { get; } = "";
+        public static string MySQLKey_Database { get; } = "";
+
+        public static string PromoitCampaignQueue { get; } = "";
+        public static string PromoitProductQueue { get; } = "";
+        public static string PromoitTweetQueue { get; } = "";
+        public static string SetUserQueue { get; } = "";
+
+        public static string PromoitCampaignFunctions { get; } = "";
+        public static string PromoitProductFunctions { get; } = "";
+        public static string PromoitTweetFunctions { get; } = "";
+        public static string SetUserFunctions { get; } = "";
+
+
+        public static string APIKey { get; } = "";
+        public static string APISecret { get; } = "";
+        public static string APIToken { get; } = "";
+        public static string APITokenSecret { get; } = "";
+
+
+
+
+
+    }
+}
